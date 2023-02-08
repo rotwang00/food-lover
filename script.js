4a439dabeccf220a092d5d8ab1380ca73cacb906
Paul Thurmond <paulthurmond@gmail.com>
2023-02-08 15:46:04 +0000

    Refactor card display

--- script.js
+++ script.js
@@ -17,39 +17,66 @@ let recipes = [
 
 let cards = document.getElementById("cards");
 
-const createCard = function (dish, ingredients, instructions) {
-  let newCard = document.createElement("div");
+const createCards = function (recipes) {
+  for (let recipe of recipes) {
+    let newCard = document.createElement("div");
 
-  let recipeTitle = document.createElement("h3");
-  recipeTitle.textContent = dish;
-  newCard.append(recipeTitle);
+    let recipeTitle = document.createElement("h3");
+    recipeTitle.textContent = recipe.title;
+    newCard.append(recipeTitle);
 
-  let ingredientList = document.createElement("ul");
-  for (let ingredient of ingredients) {
-    let newIngredient = document.createElement("li");
-    newIngredient.textContent = ingredient;
-    ingredientList.append(newIngredient);
+    let ingredientList = document.createElement("ul");
+    for (let ingredient of recipe.ingredients) {
+      let newIngredient = document.createElement("li");
+      newIngredient.textContent = ingredient;
+      ingredientList.append(newIngredient);
+    }
+    newCard.append(ingredientList);
+
+    let instructionList = document.createElement("ol");
+    for (let step of recipe.instructions) {
+      let newStep = document.createElement("li");
+      newStep.textContent = step;
+      instructionList.append(newStep);
+    }
+    newCard.append(instructionList);
+
+    newCard.classList.add("card");
+
+    cards.append(newCard);
   }
-  newCard.append(ingredientList);
-
-  let instructionList = document.createElement("ol");
-  for (let step of instructions) {
-    let newStep = document.createElement("li");
-    newStep.textContent = step;
-    instructionList.append(newStep);
-  }
-  newCard.append(instructionList);
-
-  newCard.classList.add("card");
-
-  cards.append(newCard);
 };
 
-for (let recipe of recipes) {
-  createCard(recipe.title, recipe.ingredients, recipe.instructions);
-}
-// createCard(
-//   "Lasagna",
-//   ["noodles", "sauce", "cheese", "sausage"],
-//   ["Cook the noodles", "Layer everything", "Bake that sucker"]
-// );
+createCards(recipes);
+
+// const createCard = function (dish, ingredients, instructions) {
+//   let newCard = document.createElement("div");
+
+//   let recipeTitle = document.createElement("h3");
+//   recipeTitle.textContent = dish;
+//   newCard.append(recipeTitle);
+
+//   let ingredientList = document.createElement("ul");
+//   for (let ingredient of ingredients) {
+//     let newIngredient = document.createElement("li");
+//     newIngredient.textContent = ingredient;
+//     ingredientList.append(newIngredient);
+//   }
+//   newCard.append(ingredientList);
+
+//   let instructionList = document.createElement("ol");
+//   for (let step of instructions) {
+//     let newStep = document.createElement("li");
+//     newStep.textContent = step;
+//     instructionList.append(newStep);
+//   }
+//   newCard.append(instructionList);
+
+//   newCard.classList.add("card");
+
+//   cards.append(newCard);
+// };
+
+// for (let recipe of recipes) {
+//   createCard(recipe.title, recipe.ingredients, recipe.instructions);
+// }
